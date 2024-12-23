afd79125adfbc1ce4b04c2cca27dd15a8a030769
Shraddha Karne <suyashkarne294@gmail.com>
2024-12-23 14:46:56 +0530

    compare ss added

---
+++ tests/compareScreenshots.spec.js
@@ -0,0 +1,13 @@
+import {test,expect} from '@playwright/test'
+
+test('Compare Screenshot',async({page})=>{
+    await page.goto("https://retail.onlinesbi.sbi/npersonal/faq.html");
+
+    //compare full page ss
+    expect (await page.screenshot()).toMatchSnapshot('sbiFaq.png');
+
+    //compare specific element ss
+    expect (await page.locator("//header[@data-include='header']").screenshot()).toMatchSnapshot('faqHeader.png');
+
+    await page.close();
+})
