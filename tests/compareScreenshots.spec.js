3e9f3bccf279c892754c556662d514ec0a7fe3d9
suyashkarne <41177906+suyashkarne@users.noreply.github.com>
2024-12-23 21:41:45 +0530

    Update compareScreenshots.spec.js

--- tests/compareScreenshots.spec.js
+++ tests/compareScreenshots.spec.js
@@ -3,7 +3,7 @@ import {test,expect} from '@playwright/test'
 test('Compare Screenshot',async({page})=>{
     await page.goto("https://retail.onlinesbi.sbi/npersonal/faq.html");
 
-    //compare full page ss
+    //compare full page ss of sbi faq
     expect (await page.screenshot()).toMatchSnapshot('sbiFaq.png');
 
     //compare specific element ss
